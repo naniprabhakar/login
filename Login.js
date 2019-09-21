56d734c0df463cbfe9a532da3dfa8a81da94aad2
naniprabhakar <55038229+naniprabhakar@users.noreply.github.com>
2019-09-21 15:36:04 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,139 @@
+import React from 'react';
+
+
+class Login extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        fields: {},
+        errors: {}
+      }
+
+      this.handleChange = this.handleChange.bind(this);
+      this.submitLoginForm = this.submitLoginForm.bind(this);
+
+    };
+
+    handleChange(e) {
+      let fields = this.state.fields;
+      fields[e.target.name] = e.target.value;
+      this.setState({
+        fields
+      });
+
+    }
+
+    submitLoginForm(e) {
+      e.preventDefault();
+      if (this.validateForm()) {
+          let fields = {};
+          fields["username"] = "";
+          fields["emailid"] = "";
+          fields["mobileno"] = "";
+          fields["password"] = "";
+          this.setState({fields:fields});
+          alert("Form submitted");
+      }
+
+    }
+
+    validateForm() {
+
+      let fields = this.state.fields;
+      let errors = {};
+      let formIsValid = true;
+
+      if (!fields["username"]) {
+        formIsValid = false;
+        errors["username"] = "*Please enter your username.";
+      }
+
+      if (typeof fields["username"] !== "undefined") {
+        if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
+          formIsValid = false;
+          errors["username"] = "*Please enter alphabet characters only.";
+        }
+      }
+
+      if (!fields["emailid"]) {
+        formIsValid = false;
+        errors["emailid"] = "*Please enter your email-ID.";
+      }
+
+      if (typeof fields["emailid"] !== "undefined") {
+        //regular expression for email validation
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        if (!pattern.test(fields["emailid"])) {
+          formIsValid = false;
+          errors["emailid"] = "*Please enter valid email-ID.";
+        }
+      }
+
+      if (!fields["mobileno"]) {
+        formIsValid = false;
+        errors["mobileno"] = "*Please enter your mobile no.";
+      }
+
+      if (typeof fields["mobileno"] !== "undefined") {
+        if (!fields["mobileno"].match(/^[0-9]{10}$/)) {
+          formIsValid = false;
+          errors["mobileno"] = "*Please enter valid mobile no.";
+        }
+      }
+
+      if (!fields["password"]) {
+        formIsValid = false;
+        errors["password"] = "*Please enter your password.";
+      }
+
+      if (typeof fields["password"] !== "undefined") {
+        if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+          formIsValid = false;
+          errors["password"] = "*Please enter secure and strong password.";
+        }
+      }
+
+      this.setState({
+        errors: errors
+      });
+      return formIsValid;
+
+
+    }
+
+
+
+  render() {
+    return (
+    <div id="main-registration-container">
+     <div id="register">
+        
+    <fieldset>
+      <div class="form-1">
+        <form  method="post"  name="LoginForm"  onSubmit= {this.submitLoginForm} >
+        
+        <tr><td><label>Email ID:</label></td></tr>
+        <tr><td><input type="text" name="emailid" value={this.state.fields.emailid} onChange={this.handleChange} placeholder="Enter your Email ID"  /></td></tr>
+        <tr><div className="errorMsg">{this.state.errors.emailid}</div></tr>
+        
+        <tr><td><label>Password:</label></td></tr>
+        <tr><input type="password" name="password" value={this.state.fields.password} onChange={this.handleChange} placeholder="Enter your password"/>
+        <div className="errorMsg">{this.state.errors.password}</div></tr>
+        <input type="submit" className="button"  value="Login"/>
+        <a href="#" id="forgot">Forgot Password?</a>
+    
+       </form>
+       </div>
+       </fieldset>
+        
+    </div>
+</div>
+
+      );
+  }
+
+
+}
+
+
+export default Login;
