1a8eeb647284d760c9f201bc78d4c7f742deded9
naniprabhakar <55038229+naniprabhakar@users.noreply.github.com>
2019-09-21 15:37:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Login from './Login.js';
+//import './Style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>student login page</h1>
+      <Login/>
+
+    </div>
+  );
+}
+
+export default App;
+
